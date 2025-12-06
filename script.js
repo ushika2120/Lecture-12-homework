1755ea5163fb2ad2ace701999f8bc0aa4b28acc8
ushika2120 <uleush11@gmail.com>
2025-12-07 02:02:46 +0400

    Lecture 12 homework

---
+++ script.js
@@ -0,0 +1,28 @@
+let Name = "Levan"
+
+console.log("Name");
+
+let Surname = "Ushikishvili"
+
+console.log( "Surname");
+
+let Age = 42
+ 
+let Age1 = 17
+
+let result = Age > Age1 
+
+console.warn(result);
+
+let Mail = "uleush11@gmail.com"
+
+console.log("Mail");
+
+Number = "500000000"
+
+console.error(Number);
+
+Boolean = "true" || "false"
+
+
+ 
